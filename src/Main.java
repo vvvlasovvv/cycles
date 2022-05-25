8d29322c9baf1800cf07b5bf84d9ceeba14e1e33
Alexey <vlasovalexey13@gmail.com>
2022-05-25 22:50:16 +0300

    Выполниил ДЗ по циклам

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Задание №1
+        int currentNum = 0;
+
+        while (currentNum < 10) {
+
+            System.out.print(++currentNum + " ");
+        }
+
+        System.out.println();
+
+        for (; currentNum > 0; currentNum--) {
+            System.out.print (currentNum + " ");
+        }
+
+        System.out.println();
+
+
+        //Задание №2
+        int firstFriday = 5;
+
+        for (int currentFriday = firstFriday; currentFriday <= 31; currentFriday += 7) {
+            System.out.println("Сегодня пятница, " + currentFriday + "-е число. Необходимо подготовить отчет");
+        }
+
+        //Задание №3
+        int currentYear = LocalDate.now().getYear();
+
+        int start = currentYear - 200;
+        int end = currentYear + 200;
+
+        for (int year = start; year <= end; year++) {
+            if (year % 79 == 0) {
+                System.out.println(year);
+            }
+        }
+
     }
 }
